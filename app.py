27ac31eb052abd14fc3d305d43626aa53f96dc16
RayhanRich4rd <rayhanrichardtanjong@gmail.com>
2023-11-08 11:49:41 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import (
     Flask,
     request,
@@ -12,13 +16,17 @@ import requests
 from bson import ObjectId
 from datetime import datetime
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+
 app = Flask(__name__)
-
-password = 'sparta'
-cxn_str = f'mongodb+srv://test:{password}@cluster0.g4dogux.mongodb.net/'
-client = MongoClient(cxn_str)
-
-db = client.dbsparta_plus_week2
 
 
 @app.route('/')
